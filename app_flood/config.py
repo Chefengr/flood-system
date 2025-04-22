1556d61687a440f2a984decf68984dbb1a24492a
Chefengr <135500943+Chefengr@users.noreply.github.com>
2025-04-22 15:21:27 +0800

    Delete app_flood directory

--- app_flood/config.py
+++
@@ -1,6 +0,0 @@
-db_config = {
-    'host': os.environ.get('DB_HOST', 'localhost'),
-    'user': os.environ.get('DB_USER', 'root'),
-    'password': os.environ.get('DB_PASSWORD', ''),
-    'database': os.environ.get('DB_NAME', 'flood_detection')
-}
