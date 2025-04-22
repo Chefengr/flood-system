c01db3b92985542c1a8cb053dfe2eb55c213d7ff
Chefengr <135500943+Chefengr@users.noreply.github.com>
2025-04-22 17:30:46 +0800

    Update __init__.py

--- app_flood/__init__.py
+++ app_flood/__init__.py
@@ -1,7 +1,6 @@
-# app_flood/__init__.py
 from flask import Flask
 
 app = Flask(__name__)
 
-# Import routes (if split into separate files)
-from app_flood import routes  # Optional
+# Remove this line: "from app_flood import routes"
+# Instead, import routes AFTER creating the app (or inside a function)
