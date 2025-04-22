b966128eb4187db67b85dfa8338adca58af4e993
Chefengr <135500943+Chefengr@users.noreply.github.com>
2025-04-22 17:38:08 +0800

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
-from app_flood import app  # Imports the Flask app from app_flood/__init__.py
+from app_flood import app
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)  # Optional: Set explicit host/port
+    app.run()
